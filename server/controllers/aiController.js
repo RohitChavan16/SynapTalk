a91d27898e85ad9fe537988b56f4122f50e0374e
Rohit Chavan <approachrohit16@gmail.com>
2025-12-05 14:58:11 +0530

    Changing gemini model as last model free trail expired

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -21,7 +21,7 @@ export const handleAIMessage = async (req, res) => {
     }
 
     const model = genAI.getGenerativeModel({ 
-      model: "gemini-2.0-flash-exp"
+      model: "gemini-2.5-flash-lite"
     });
     
     const result = await model.generateContent(aiQuery);
